1f4d35fafe5fa262b5f8f6cfe704e369e659110a
Zack Berger <zberger@my.smccd.edu>
2025-11-12 00:47:09 -0500

    Initial Commit

---
+++ HOME WORK 13 PROGRESS VIEW ZACK BERGER/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HOME WORK 13 PROGRESS VIEW ZACK BERGER
+//
+//  Created by Zachary Todd Berger on 11/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HOME WORK 13 PROGRESS VIEW ZACK BERGER/HOME_WORK_13_PROGRESS_VIEW_ZACK_BERGERApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOME_WORK_13_PROGRESS_VIEW_ZACK_BERGERApp.swift
+//  HOME WORK 13 PROGRESS VIEW ZACK BERGER
+//
+//  Created by Zachary Todd Berger on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HOME_WORK_13_PROGRESS_VIEW_ZACK_BERGERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
